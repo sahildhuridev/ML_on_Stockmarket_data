7235819cee72f9146ada0507259315846337ce6b
sahildhuridev <sahildhuri.dev216@gmail.com>
2026-03-13 02:32:49 +0530

    made change for ci cd pipeline

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@^2&af(9gq+_+$9x8u*fy(g%7(axvj@e)3^2*0j!txmwzgva&3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
