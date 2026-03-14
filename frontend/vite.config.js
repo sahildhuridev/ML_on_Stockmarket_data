d7d16f936c4a2c89fbf484bf3b743868c8865b6a
sahildhuridev <sahildhuri.dev216@gmail.com>
2026-03-14 15:57:28 +0530

    keeping domain name in allowed host as it gives 502 gateway error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: "0.0.0.0",
     port: 5173,
-    allowedHosts: "all"
+    allowedHosts: [
+      "ml.sahildhuri.co.in",
+      "localhost",
+      "127.0.0.1"
+    ]
   }
 })
